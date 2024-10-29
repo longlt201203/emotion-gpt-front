df8676dc99d8e7ae2a406b073a8d8c6e915482c0
Le Thanh Long <longlt201203@gmail.com>
2024-10-29 20:24:46 +0700

    feat: 🎸 update

--- src/pages/ChatPage/Sidebar.tsx
+++ src/pages/ChatPage/Sidebar.tsx
@@ -1,13 +1,15 @@
 import { Separator } from "@/components/ui/separator";
 import Profile from "./Profile";
 import ChatHistory from "./ChatHistory";
+import { Button } from "@/components/ui/button";
 
 export default function Sidebar() {
-    return (
-        <div className="border-r p-4 flex flex-col gap-4">
-            <Profile/>
-            <Separator/>
-            <ChatHistory/>
-        </div>
-    );
+  return (
+    <div className="border-r p-4 flex flex-col gap-4">
+      <Profile />
+      <Button>New Chat</Button>
+      <Separator />
+      <ChatHistory />
+    </div>
+  );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,26 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import ChatPage from "./pages/ChatPage";
-import LoginPageV2 from "./pages/LoginPageV2";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        children: [
-            {
-                path: "",
-                element: <ChatPage/>
-            },
-            {
-                path: "login",
-                element: <LoginPage/>
-            },
-            {
-                path: "login-v2",
-                element: <LoginPageV2/>
-            }
-        ]
-    }
+  {
+    path: "/",
+    children: [
+      {
+        path: "",
+        element: <ChatPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+    ],
+  },
 ]);
 
 export default router;
